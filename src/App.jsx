1471acddf646e11d57265cf207a9858b709ec91d
JWillett05 <jack.yuze@uk>
2024-09-10 13:23:34 +0100

    Completed CSS for buttons

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Index from './index.js'
+import Header from './Header.jsx';
+import CalcButtons from './CalcButtons.jsx';
+
+function App(){
+    
+    return(
+        <>
+        <Header />
+        <CalcButtons />
+        </>
+    );
+}
+
+export default App
---
+++ src/CalcButtons.jsx
@@ -0,0 +1,28 @@
+export default function CalcButtons(){
+    return(
+        <div id="calc">
+            <div id="buttons">
+                <div id="button-char">1</div>
+                <div id="button-char">2</div>
+                <div id="button-char">3</div>
+                <div id="button-char">%</div>
+                <div id="button-char">4</div>
+                <div id="button-char">5</div>
+                <div id="button-char">6</div>
+                <div id="button-char">X</div>
+                <div id="button-char">7</div>
+                <div id="button-char">8</div>
+                <div id="button-char">9</div>
+                <div id="button-char">-</div>
+                <div id="button-char">0</div>
+                <div id="button-char">.</div>
+                <div id="button-char">=</div>
+                <div id="button-char">+</div>
+
+            </div>
+        </div>
+
+        
+
+    )
+}
